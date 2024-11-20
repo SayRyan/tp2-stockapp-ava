d203e93f932990de7d36e69dc21d86a383c088ab
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-11-20 11:10:09 -0300

    Criação da Interface Supplier na camada Domain

---
+++ StockApp.Domain/Interfaces/ISupplierRepository.cs
@@ -0,0 +1,18 @@
+﻿using StockApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Domain.Interfaces
+{
+    public interface ISupplierRepository
+    {
+        Task<IEnumerable<Supplier>> GetSuppliers();
+        Task<Supplier> GetById(int id);
+        Task<Supplier> Create(Supplier supplier);
+        Task<Supplier> Update(Supplier supplier);
+        Task<Supplier> Remove(Supplier supplier);
+    }
+}
