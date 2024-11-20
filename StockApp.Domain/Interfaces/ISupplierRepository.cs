6ff3408c8705fe0a07a389f26599307c83983fa1
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-11-20 11:37:42 -0300

    Implementação Repository Supplier na camada InfraData

--- StockApp.Domain/Interfaces/ISupplierRepository.cs
+++ StockApp.Domain/Interfaces/ISupplierRepository.cs
@@ -10,7 +10,7 @@ namespace StockApp.Domain.Interfaces
     public interface ISupplierRepository
     {
         Task<IEnumerable<Supplier>> GetSuppliers();
-        Task<Supplier> GetById(int id);
+        Task<Supplier> GetById(int? id);
         Task<Supplier> Create(Supplier supplier);
         Task<Supplier> Update(Supplier supplier);
         Task<Supplier> Remove(Supplier supplier);
---
+++ StockApp.Infra.Data/Repositories/SupplierRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockApp.Domain.Entities;
+using StockApp.Domain.Interfaces;
+using StockApp.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Infra.Data.Repositories
+{
+    public class SupplierRepository : ISupplierRepository
+    {
+        ApplicationDbContext _supplierContext;
+        public SupplierRepository(ApplicationDbContext context)
+        {
+            _supplierContext = context;
+        }
+
+        public async Task<Supplier> Create(Supplier supplier)
+        {
+            _supplierContext.Add(supplier);
+            await _supplierContext.SaveChangesAsync();
+            return supplier;
+        }
+
+        public async Task<Supplier> GetById(int? id)
+        {
+            var supplier = await _supplierContext.Suppliers.FindAsync(id);
+            return supplier;
+        }
+
+        public async Task<IEnumerable<Supplier>> GetSuppliers()
+        {
+            return await _supplierContext.Suppliers.OrderBy(s => s.Name).ToListAsync();
+        }
+
+        public async Task<Supplier> Update(Supplier supplier)
+        {
+            _supplierContext.Update(supplier);
+            await _supplierContext.SaveChangesAsync();
+            return supplier;
+        }
+
+        public async Task<Supplier> Remove(Supplier supplier)
+        {
+            _supplierContext.Remove(supplier);
+            await _supplierContext.SaveChangesAsync();
+            return supplier;
+        }
+    }
+}
