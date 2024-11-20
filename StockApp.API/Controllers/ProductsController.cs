d06f7ce71910c78a402b0c30b943e2a49fb01430
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-11-20 00:50:40 -0300

    Configuração de Cors - Testado com uma página de Front-End para fazer requisição na API.

---
+++ StockApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    [EnableCors("AllowSpecificOrigins")]
+    [Route("/api/[controller]")]
+    [ApiController]
+
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+    }
+}
